d8d84a1674a47d56f9d5c7f1fcdd02ea2f1a78a5
Prathamesh Kulkarni <prathameshkulkarni295@gmail.com>
2026-03-09 00:58:32 +0530

    Fix port for deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ connectDB();
 
 // Initialize Express app
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Import routes
 const receiptRoute = require("./routes/receipt");
